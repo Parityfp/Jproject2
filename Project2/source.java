19c0e24740db25a737e811ab14b12c11af6a80a3
NineDERP <132637581+NineDERP@users.noreply.github.com>
2023-11-09 09:08:13 +0700

    Add files via upload

--- Project2/source.java
+++ Project2/source.java
@@ -19,35 +19,55 @@ class Material {
     }
 }
 
-class SupplierThread extends Thread {
+abstract class MyAbstractThread extends Thread
+{
+    protected  int		rounds;
+    protected  CyclicBarrier	cfinish;
 
+    public MyAbstractThread(String name)		{ super(name); }
+
+    public void setRounds(int r)			{ rounds = r; }
+    public void setCyclicBarrier(CyclicBarrier f)	{ cfinish = f; }
+    
+
+    @Override
+    public void run()
+    {
+        System.out.println(Thread.currentThread().getName());
+    }
+};
+class SupplierThread extends MyAbstractThread {
+ public SupplierThread(String name)		{ super(name); }
 }
 
-class FactoryThread extends Thread {
+class FactoryThread extends MyAbstractThread {
 
     public int lots;
     public String product;
 
-    public FactoryThread(String product, int lots){ 
-        
+    public FactoryThread(String name,String p, int l){ 
+        super(name);
+        lots = 0;
+        product = p;
     }
 }
 
-public class source {
+ class source {
     static ArrayList<Material> AllMaterials = new ArrayList<>();
     static ArrayList<ArrayList<String>> AllSuppliers = new ArrayList<>();
     static ArrayList<ArrayList<String>> AllFactories = new ArrayList<>();
     static int days;
-
+    static int parties = 0;
     public static void main(String[] args) {
-        System.out.printf("%-15s>> Enter config file for simulation =\n",Thread.currentThread().getName());
         ReadConfig();
         System.out.println(days + "\n" + AllMaterials.toString() + "\n" + AllSuppliers + "\n" + AllFactories);
+        CyclicBarrier barrier = new CyclicBarrier(parties);
     }
 
     static void ReadConfig(){
         //for codebeans
-        String path = "src/main/Java/Project2/", filename = "config";
+        
+        String path = "src/main/Java/Project2/", filename = "config.txt";
         Scanner keyboardScan = new Scanner(System.in);
         //for vscode
         //String inputFile = "C:\\Users/person/Desktop/Coding/Java/paradigms/src/Project2/config.txt";
@@ -68,17 +88,18 @@ public class source {
                     for(int i = 1; i < col.length; i++){
                         supplier.add(col[i].trim()); 
                     }
-                    AllSuppliers.add(supplier);    
+                    AllSuppliers.add(supplier); parties++;    
                     break;
                 case("F"):ArrayList<String> factory = new ArrayList<>();
                     for(int i = 1; i < col.length; i++){
                         factory.add(col[i].trim()); 
                     }
-                    AllFactories.add(factory); 
+                    AllFactories.add(factory); parties++;
                     break;
             }
             }
         fscanner.close();
+       
         
         }catch (Exception e) {
             System.out.println();
