db4c78ce5e10b2a85301805c7a3badb2f1db4aa3
NineDERP <132637581+NineDERP@users.noreply.github.com>
2023-11-09 11:43:18 +0700

    Add files via upload

--- Project2/source.java
+++ Project2/source.java
@@ -18,22 +18,40 @@ class Material {
         return this.ID;
     }
 }
+/*
+to do list:
+- create wait/notify for waiting conditions 
+- cyclic barrier for only the factorythreads 
+- main thread report before day 1 
+- threads activity
+- summary on main 
 
+*/
 abstract class MyAbstractThread extends Thread
 {
-    protected  int		rounds;
+    protected  int		rounds,rates;
     protected  CyclicBarrier	cfinish;
+    protected Random random;
 
-    public MyAbstractThread(String name)		{ super(name); }
+    public MyAbstractThread(String name)		{ super(name);  random = new Random();}
 
     public void setRounds(int r)			{ rounds = r; }
     public void setCyclicBarrier(CyclicBarrier f)	{ cfinish = f; }
     
 
     @Override
-    public void run()
+    synchronized public  void run()
     {
-        System.out.println(Thread.currentThread().getName());
+        
+        for(int i=1; i<=rounds;i++){
+        try {
+            //Thread.sleep(random.nextInt(100,200));
+            System.out.println(Thread.currentThread().getName());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        try { if(cfinish.getNumberWaiting() == 0 ) notify(); cfinish.await();  } catch (Exception e) { }
+    }
     }
 };
 class SupplierThread extends MyAbstractThread {
@@ -45,7 +63,7 @@ class FactoryThread extends MyAbstractThread {
     public int lots;
     public String product;
 
-    public FactoryThread(String name,String p, int l){ 
+    public FactoryThread(String name,String p){ 
         super(name);
         lots = 0;
         product = p;
@@ -56,12 +74,15 @@ class FactoryThread extends MyAbstractThread {
     static ArrayList<Material> AllMaterials = new ArrayList<>();
     static ArrayList<ArrayList<String>> AllSuppliers = new ArrayList<>();
     static ArrayList<ArrayList<String>> AllFactories = new ArrayList<>();
+    static ArrayList<FactoryThread> AllFthreads = new ArrayList <>();
+    static ArrayList<SupplierThread> AllSthreads = new ArrayList <>();
     static int days;
     static int parties = 0;
     public static void main(String[] args) {
         ReadConfig();
-        System.out.println(days + "\n" + AllMaterials.toString() + "\n" + AllSuppliers + "\n" + AllFactories);
-        CyclicBarrier barrier = new CyclicBarrier(parties);
+        source sim = new source();
+        sim.runsimulation();
+        
     }
 
     static void ReadConfig(){
@@ -88,13 +109,19 @@ class FactoryThread extends MyAbstractThread {
                     for(int i = 1; i < col.length; i++){
                         supplier.add(col[i].trim()); 
                     }
-                    AllSuppliers.add(supplier); parties++;    
+                    AllSuppliers.add(supplier); 
+                    SupplierThread S = new SupplierThread(col[1].trim());
+                    AllSthreads.add(S);
+                    parties++;    
                     break;
                 case("F"):ArrayList<String> factory = new ArrayList<>();
                     for(int i = 1; i < col.length; i++){
                         factory.add(col[i].trim()); 
                     }
-                    AllFactories.add(factory); parties++;
+                    AllFactories.add(factory); 
+                    FactoryThread F = new FactoryThread(col[1].trim(), col[2].trim());
+                    AllFthreads.add(F);
+                    parties++;
                     break;
             }
             }
@@ -108,5 +135,38 @@ class FactoryThread extends MyAbstractThread {
             filename = keyboardScan.next();
         }
     }
+    }
+    
+    synchronized public void runsimulation(){
+        System.out.println(days + "\n" + AllMaterials.toString() + "\n" + AllSuppliers + "\n" + AllFactories);
+        CyclicBarrier barrier = new CyclicBarrier(parties+1);
+         
+        for (SupplierThread S : AllSthreads){ S.setCyclicBarrier(barrier); S.setRounds(days); S.start();}
+        for (FactoryThread F : AllFthreads) { F.setCyclicBarrier(barrier); F.setRounds(days); F.start();}
+        
+        for(int i=1; i<=days;i++){
+        System.out.println(Thread.currentThread().getName() + i + barrier.getNumberWaiting());
+        try { 
+        while(barrier.getNumberWaiting()>0)
+            wait(); 
+        notifyAll();
+        }catch (Exception e) { }
+        
+        }
+        for (FactoryThread F : AllFthreads) {
+            try {
+                F.join();
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+        for (SupplierThread S : AllSthreads) {
+            try {
+                S.join();
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+        System.out.println("summary");
     }
 }
